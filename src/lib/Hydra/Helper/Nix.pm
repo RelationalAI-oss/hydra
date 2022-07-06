bc2fa37b61c9533a241f7c1bce43e191861f8bf3
Rob Vermaas <rob.vermaas@gmail.com>
2022-07-06 13:43:04 +0200

    Fix typo

--- src/lib/Hydra/Helper/Nix.pm
+++ src/lib/Hydra/Helper/Nix.pm
@@ -492,7 +492,7 @@ sub cancelBuilds {
 sub cancelBuildsForJobset {
     my ($db, $jobset_id) = @_;
     my $builds = $db->resultset('Builds')->search_rs(
-        { id => { -in => \ "select id from Builds where id in ((select id from Builds where jobset_id = $jobset_id and finished = 0) except (select build from JobsetEvalMembers where eval in (select max(id) from JobsetEvals where hasNewBuilds = 1 and jobset_id = $jobset_id group by jobset_id)))"
+        { id => { -in => \ "select id from Builds where id in ((select id from Builds where jobset_id = $jobset_id and finished = 0) except (select build from JobsetEvalMembers where eval in (select max(id) from JobsetEvals where hasNewBuilds = 1 and jobset_id = $jobset_id group by jobset_id)))" }
         });
     my $n = cancelBuilds($db, $builds);
     return $n;
